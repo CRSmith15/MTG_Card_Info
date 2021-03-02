220a9f2387143e5f717ad3642dcf9f0f49e6ad73
Chance Smith <chancesmith97@hotmail.com>
2021-03-02 02:20:25 +0000

    listed some fake cards in the console

--- lib/mtg_card_info/cli.rb
+++ lib/mtg_card_info/cli.rb
@@ -1,5 +1,18 @@
 class MtgCardInfo::CLI 
   def call 
     puts "Welcome to MTG Card Info!"
+    get_card_list
+    list_cards
+  end
+  
+  def get_card_list 
+    #get api data
+    @cards = ["Doom Blade", "Cancel", "Recall", "Plague Rats"]
+  end
+  
+  def list_cards
+    @cards.each_with_index do |card, index|
+      puts "#{index + 1} #{card}"
+    end
   end
 end
